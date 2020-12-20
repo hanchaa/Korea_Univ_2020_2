b2d04ec477654cc212c4c8969d9fa2857615019b
JuHan Cha <hanchaa@gmail.com>
2020-12-20 13:27:22 +0900

    feat(COSE214): add indexing and file saved log

--- COSE214/HW6/index.c
+++ COSE214/HW6/index.c
@@ -28,17 +28,22 @@ BIGRAM_INDEX *CreateIndexArray(void)
         temp[i].cnt = 0;
         temp[i].capacity = 100;
         temp[i].array = (int *)malloc(sizeof(int) * temp[i].capacity);
+
+        if (temp[i].array == NULL) {
+            printf("No available memory!\n");
+            assert(100);
+        }
     }
 
     return temp;
 }
 
-void DestroyIndexArray(BIGRAM_INDEX *index)
+void DestroyIndexArray(BIGRAM_INDEX *bigram)
 {
     for (int i = 0; i < MAX_2GRAM_INDEX + MAX_1GRAM_INDEX; i++)
-        free(index[i].array);
+        free(bigram[i].array);
 
-    free(index);
+    free(bigram);
 }
 
 void DestroyWords(char **words, int *words_cnt)
@@ -49,14 +54,14 @@ void DestroyWords(char **words, int *words_cnt)
     free(words);
 }
 
-void CheckCapacity(BIGRAM_INDEX *index, int idx)
+void CheckCapacity(BIGRAM_INDEX *bigram, int idx)
 {
-    if (index[idx].cnt == index[idx].capacity)
+    if (bigram[idx].cnt == bigram[idx].capacity)
     {
-        index[idx].capacity += 100;
-        index[idx].array = (int *)realloc(index[idx].array, sizeof(int) * index[idx].capacity);
+        bigram[idx].capacity += 100;
+        bigram[idx].array = (int *)realloc(bigram[idx].array, sizeof(int) * bigram[idx].capacity);
 
-        if (index[idx].array == NULL)
+        if (bigram[idx].array == NULL)
         {
             printf("No available memory!\n");
             assert(100);
@@ -64,7 +69,7 @@ void CheckCapacity(BIGRAM_INDEX *index, int idx)
     }
 }
 
-void BigramIndexing(FILE *fp, char ***words, int *words_cnt, int *words_capacity, BIGRAM_INDEX *index)
+void BigramIndexing(FILE *fp, char ***words, int *words_cnt, int *words_capacity, BIGRAM_INDEX *bigram)
 {
     char buffer[100] = "";
 
@@ -85,43 +90,51 @@ void BigramIndexing(FILE *fp, char ***words, int *words_cnt, int *words_capacity
 
         if (strlen(buffer) == 2)
         {
-            CheckCapacity(index, MAX_2GRAM_INDEX + *buffer - 'a');
+            CheckCapacity(bigram, MAX_2GRAM_INDEX + *buffer - 'a');
 
-            index[MAX_2GRAM_INDEX + *buffer - 'a'].array[index[MAX_2GRAM_INDEX + *buffer - 'a'].cnt++] = (*words_cnt)++;
+            bigram[MAX_2GRAM_INDEX + *buffer - 'a'].array[bigram[MAX_2GRAM_INDEX + *buffer - 'a'].cnt++] = *words_cnt;
 
+            (*words_cnt)++;
+            continue;
+        }
+
+        if (strlen(buffer) == 3)
+        {
+            int idx = (buffer[0] - 'a') * 26 + (buffer[1] - 'a'), first_alphabet_idx = MAX_2GRAM_INDEX + buffer[0] - 'a', second_alphabet_idx = MAX_2GRAM_INDEX + buffer[1] - 'a';
+
+            CheckCapacity(bigram, first_alphabet_idx);
+            bigram[first_alphabet_idx].array[bigram[first_alphabet_idx].cnt++] = (*words_cnt);
+
+            if (first_alphabet_idx != second_alphabet_idx)
+            {
+                CheckCapacity(bigram, second_alphabet_idx);
+                bigram[second_alphabet_idx].array[bigram[second_alphabet_idx].cnt++] = (*words_cnt);
+            }
+
+            CheckCapacity(bigram, idx);
+            bigram[idx].array[bigram[idx].cnt++] = (*words_cnt);
+
+            (*words_cnt)++;
             continue;
         }
 
         int check[MAX_2GRAM_INDEX] = {0};
         for (int i = 0; i < strlen(buffer) - 2; i++)
         {
-            int j = (buffer[i] - 'a') * 26 + (buffer[i + 1] - 'a');
-            if (check[j])
+            int idx = (buffer[i] - 'a') * 26 + (buffer[i + 1] - 'a');
+            if (check[idx])
                 continue;
-            check[j] = 1;
+            check[idx] = 1;
 
-            if (strlen(buffer) == 3)
-            {
-                int first_alphabet_idx = MAX_2GRAM_INDEX + *buffer - 'a', second_alphabet_idx = MAX_2GRAM_INDEX + *(buffer + 1) - 'a';
-
-                CheckCapacity(index, first_alphabet_idx);
-                index[first_alphabet_idx].array[index[first_alphabet_idx].cnt++] = (*words_cnt);
-
-                if (first_alphabet_idx != second_alphabet_idx) {
-                    CheckCapacity(index, second_alphabet_idx);
-                    index[second_alphabet_idx].array[index[second_alphabet_idx].cnt++] = (*words_cnt);
-                }
-            }
-
-            CheckCapacity(index, j);
-            index[j].array[index[j].cnt++] = (*words_cnt);
+            CheckCapacity(bigram, idx);
+            bigram[idx].array[bigram[idx].cnt++] = (*words_cnt);
         }
 
         (*words_cnt)++;
     }
 }
 
-void WriteFile(char **words, BIGRAM_INDEX *index)
+void WriteFile(char **words, BIGRAM_INDEX *bigram)
 {
     for (int i = 0; i < MAX_2GRAM_INDEX + MAX_1GRAM_INDEX; i++)
     {
@@ -135,10 +148,11 @@ void WriteFile(char **words, BIGRAM_INDEX *index)
 
         FILE *fp = fopen(filename, "w");
 
-        for (int j = 0; j < index[i].cnt; j++)
-            fprintf(fp, "%s", words[index[i].array[j]]);
+        for (int j = 0; j < bigram[i].cnt; j++)
+            fprintf(fp, "%s", words[bigram[i].array[j]]);
 
         fclose(fp);
+        printf("%s saved\n", filename);
     }
 }
 
@@ -168,15 +182,17 @@ int main(int argc, char **argv)
         assert(100);
     }
 
-    BIGRAM_INDEX *index = CreateIndexArray();
+    BIGRAM_INDEX *bigram = CreateIndexArray();
 
-    BigramIndexing(fp, &words, &words_cnt, &words_capacity, index);
+    printf("Start 2-gram indexing\n");
+    BigramIndexing(fp, &words, &words_cnt, &words_capacity, bigram);
+    printf("2-gram indexing done\n");
 
     fclose(fp);
 
-    WriteFile(words, index);
+    WriteFile(words, bigram);
 
-    DestroyIndexArray(index);
+    DestroyIndexArray(bigram);
     DestroyWords(words, &words_cnt);
 
     return 0;
